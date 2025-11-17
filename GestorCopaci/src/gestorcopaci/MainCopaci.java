a743b941c0641f214b358658e4f474d473ba7e37
Manuel Sanchez <josemanuelsanchez096@gmail.com>
2025-11-17 13:38:05 -0600

    La clase principal que estoy usando es está, para mantener el nombre

---
+++ GestorCopaci/src/gestorcopaci/MainCopaci.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gestorcopaci;
+
+/**
+ *
+ * @author manue
+ */
+public class MainCopaci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Este main puede redirigir al login
+        Login login = new Login();
+        login.setVisible(true);
+    }
+    }
+    
